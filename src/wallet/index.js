e25628ef5c16d8bcc892c92e8c75d77962faf246
Andrew Drummond <addrummond61@gmail.com>
2018-09-26 16:59:10 -0400

    initialized wallet capability

---
+++ src/wallet/index.js
@@ -0,0 +1,91 @@
+import { ec } from 'elliptic'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
+import { _ } from 'lodash'
+import { getPublicKey, getTransactionId, signTxIn, Transaction, TxIn, TxOut } from './transaction'
+
+const EC = new ec('secp256k1');
+const privateKeyLocation = '../../wallet/private_key.txt';
+
+export function getPrivateFromWallet() {
+  const buffer = readFileSync(privateKeyLocation, 'utf8');
+  return buffer.toString();
+}
+
+export function getPublicFromWallet() {
+  const privateKey = getPrivateFromWallet();
+  const key = EC.keyFromPrivate(privateKey, 'hex');
+  return key.getPublic().encode('hex');
+}
+
+export function generatePrivateKey() {
+  const keyPair = EC.genKeyPair();
+  const privateKey = keyPair.getPrivate();
+  return privateKey.toString(16);
+}
+
+export function initWallet() {
+  if(existsSync(privateKeyLocation)) {
+    return;
+  }
+  const newPrivateKey = generatePrivateKey();
+  writeFileSync(privateKeyLocation, newPrivateKey);
+  console.log('new wallet with private key created');
+}
+
+export function getBalance(address, unspentTxOuts) {
+  return _(unspentTxOuts)
+    .filter(uTxO => uTxO.address === address)
+    .map(uTxO => uTxO.amount)
+    .sum();
+}
+
+export function findTxOutsForAmount(amount, myUnspentTxOuts) {
+  let currentAmount = 0;
+  const includedUnspentTxOuts = [];
+  for(const myUnspentTxOut of myUnspentTxOuts) {
+    includedUnspentTxOuts.push(myUnspentTxOut);
+    if(currentAmount >= amount) {
+      const leftOverAmount = currentAmount - amount;
+      return { includedUnspentTxOuts, leftOverAmount };
+    }
+  }
+  throw Error('not enough coins to send transaction');
+}
+
+export function createTxOuts(receiverAddress, myAddress, amount, leftOverAmount) {
+  const txOut1 = new TxOut(receiverAddress, amount);
+  if(leftOverAmount === 0) {
+    return [txOut1];
+  } else {
+    const leftOverTx = new TxOut(myAddress, leftOverAmount);
+    return [txOut1, leftOverTx];
+  }
+}
+
+export function toUnsignedTxIn(unspentTxOut) {
+  const txIn = new TxIn();
+  txIn.txOutId = unspentTxOut.txOutId;
+  txIn.txOutIndex = unspentTxOut.txOutIndex;
+  return txIn;
+}
+
+export function createTransaction(receiverAddress, amount, privateKey, unspentTxOuts) {
+  const myAddress = getPublicKey(privateKey);
+  const myUnspentTxOuts = unspentTxOuts.filter(uTxO => uTxO.address === myAddress);
+  const { includedUnspentTxOuts, leftOverAmount } = findTxOutsForAmount(amount, myUnspentTxOuts);
+
+  const unsignedTxIns = includedUnspentTxOuts.map(toUnsignedTxIn);
+
+  const tx = new Transaction();
+  tx.txIns = unsignedTxIns;
+  tx.txOuts = createTxOuts(receiverAddress, myAddress, amount, leftOverAmount);
+  tx.id = getTransactionId(tx);
+
+  tx.txIns = tx.txIns.map((txIn, index) => {
+    txIn.signature = signTxIn(tx, index, privateKey, unspentTxOuts);
+    return txIn;
+  });
+
+  return tx;
+
+}
---
+++ src/wallet/transaction.js
@@ -0,0 +1,80 @@
+import CryptoJS from 'crypto-js'
+import ecdsa from 'elliptic'
+import _ from 'lodash'
+
+const ec = new ecdsa.ec('secp256k1');
+
+/*
+TRANSACTION CLASS DECLARATIONS
+*/
+export class TxOut {
+  constructor(address, amount) {
+    this.address = address;
+    this.amount = amount;
+  }
+}
+
+export class TxIn {
+  constructor() {
+    this.txOutId = null;
+    this.txOutIndex = null;
+    this.signature = null;
+  }
+}
+
+export class Transaction {
+  constructor() {
+    this.id = null;
+    this.txIns = [];
+    this.txOuts = [];
+  }
+}
+
+/*
+HELPER METHODS
+*/
+export function getTransactionId(transaction) {
+  const txInContent = transaction.txIns
+    .map(txIn => txIn.txOutId + txIn.txOutIndex)
+    .reduce((a, b) => a + b, '');
+
+  const txOutContent = transaction.txOuts
+    .map(txOut => txOut.address + txOut.amount)
+    .reduce((a, b) => a + b, '');
+
+  return CryptoJS.SHA256(txInContent + txOutContent).toString();
+}
+
+
+export function signTxIn(transaction, txInIndex, txInNumber, privateKey, aUnspentTxOuts) {
+  const txIn = transaction.txIns[txInIndex];
+  const dataToSign = transaction.id;
+  const referencedUnspentTxOut = findUnspentTxOut(txIn.txOutId, txIn.txOutIndex, aUnspentTxOuts);
+  if(referencedUnspentTxOut === null) {
+    console.log('could not find referenced unspent transaction output');
+    throw Error('No transaction output found');
+  }
+  const referencedAddress = referencedUnspentTxOut.address;
+
+  if(getPublicKey(privateKey) !== referencedAddress) {
+    console.log('trying to sign an input with private key that does not match'
+                + 'the address that is referenced in the txIn');
+    throw Error('private key does not match referenced address');
+  }
+  const key = ec.keyFromPrivate(privateKey, 'hex');
+  const signature = toHexString(key.sign(dataToSign).toDER());
+  return signature;
+}
+
+
+export function toHexString(byteArray) {
+  return Array.from(byteArray, byte => {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('');
+}
+
+export function getPublicKey(aPrivateKey) {
+  return ec.keyFromPrivate(aPrivateKey, 'hex').getPublic().encode('hex');
+}
+
+
